aeda976cfbdafa4411fc563c3cd0c0846ccb3162
MaxHsuProgram <maxhsu0103@gmail.com>
2020-03-22 22:04:54 +0800

    a031

---
+++ GreenJudge/a031.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    long long int A , B;
+    cin >> A >> B;
+    if ( A > B )
+    {
+        cout << "A" << " " << A + B << endl;
+    }
+    else
+    {
+        cout << "B" << " " << A + B << endl;
+    }
+}
