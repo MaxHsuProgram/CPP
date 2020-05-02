f9bcf462f354058ee284734ed7adeedc5f5c4f3a
MaxHsuProgram <maxhsu0103@gmail.com>
2020-05-02 20:45:52 +0800

    b002

---
+++ cpp_learning/範例程式/ch01/prog1_1.cpp
@@ -0,0 +1,10 @@
+// prog1_1, 第一個C++程式
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main(void)
+{
+   cout << "Far form eye, far from heart" << endl;	   // 印出Far form eye, far from heart
+   system("pause");
+   return 0;
+}
---
+++ cpp_learning/範例程式/ch06/prog6_6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+inline void star(void) 	
+   cout << "************" << endl;
+}
+
+int main(void)       		
+{
+   star();
+   cout << "Hello, C++" << endl;
+   star(); 
+   system("pause");
+}
+
+
+
+
